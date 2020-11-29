f2b900c6f8a3090412e70c3876fe07bae6821dc6
Brian <bdombro@gmail.com>
2020-11-29 08:30:47 -0600

    Ignoring repl.ts changes

--- packages/api/src/repl.ts
+++ packages/api/src/repl.ts
@@ -1,17 +1,5 @@
 // Add stuff here to experiment with libraries
-function main() {
-  const query = `lskdjflskj
-  query Posts {\\n' +
-[dev:api]     '  posts {\\n' +
-[dev:api]     '    id\\n' +
-[dev:api]     '    title\\n' +
-[dev:api]     '    author {\\n' +
-[dev:api]     '      id\\n' +
-[dev:api]     '      name\\n' +
-[dev:api]     '    }\\n' +
-[dev:api]     '`
-
-  const operationName = query.match(/(query|mutate) (.*) {/)?.[2]
-  console.dir(operationName)
+export function main() {
+  throw new Error('Nothing here!')
 }
 main()
--- packages/common/src/repl.ts
+++ packages/common/src/repl.ts
@@ -1,5 +1,5 @@
 // Add stuff here to experiment with libraries
 export function main() {
-
+  throw new Error('Nothing here!')
 }
 main()
